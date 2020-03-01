78cbc5ec60586794162ccab20156bdcd8d6a0375
Kalum <13sco17@plume.essex.sch.uk>
2020-03-01 11:54:47 +0000

    Rounded Images for bios

--- resources/views/bio/home.blade.php
+++ resources/views/bio/home.blade.php
@@ -13,7 +13,7 @@
         <article class="media">
           <div class="media-left">
             <figure class="image is-64x64">
-                <img src="{{ $user->avatar }}" alt="Image">
+                <img class="is-rounded" src="{{ $user->avatar }}" alt="Image">
             </figure>
           </div>
           <div class="media-content">
