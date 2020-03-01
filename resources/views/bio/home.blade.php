e5d91b677debbfbcc2668c77e586b350d8989d4f
Kalum <13sco17@plume.essex.sch.uk>
2020-03-01 23:56:43 +0000

    Bug fix

--- resources/views/bio/home.blade.php
+++ resources/views/bio/home.blade.php
@@ -41,38 +41,6 @@
     </div>
   </div>
 </div>
-        <div class="column is-5">
-        <div class="container" style="padding: 10px;">
-    <div class="box" style="height: 400px;overflow: auto;">
-        <article class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-                <img class="is-rounded" src="{{ $user->avatar }}" alt="Image">
-            </figure>
-          </div>
-          <div class="media-content">
-            <div class="content">
-              <p>
-                <strong>{{ $user->username }}</strong><small>#{{ $user->discrim }}</small>
-                <br />
-                <span class="tag is-primary"><i class="fas fa-clock"></i>&nbsp;{{ $user->status }}</span> @if ($user->userid == '390179632911089666') <span class="tag is-danger"><i class="fas fa-file-code"></i>&nbsp;Lead Developer</span> @endif
-                @if ($user->userid == '98093345506615296') <span class="tag is-danger"><i class="fas fa-tasks"></i></i>&nbsp;Project Leader</span> @endif
-                @if ($user->userid == '141218912934166528') <span class="tag is-danger"><i class="fas fa-file-code"></i>&nbsp;Project Manager</span> @endif
-                <br />
-              </p>
-                    <p><strong><i class="fas fa-info-circle"></i>&nbsp;Description:&nbsp;</strong> {{ $user->description }}</p>
-                    <p><strong><i class="fas fa-envelope-square"></i>&nbsp;Email:&nbsp;</strong> {{ $user->email }}</p>
-                    <p><strong><i class="far fa-calendar-times"></i>&nbsp;Date of Birth:&nbsp;</strong> {{ $user->dob }}</p>
-                    <p><strong><i class="fas fa-genderless"></i>&nbsp;Gender:&nbsp;</strong> {{ $user->gender }}</p>
-                    <p><strong><i class="fas fa-user-md"></i>&nbsp;Occupation:&nbsp;</strong> {{ $user->occupation }}</p>
-                    <p><strong><i class="far fa-plus-square"></i>&nbsp;Created At:&nbsp;</strong> {{ $user->created_at }}</p>
-                    <p><strong><i class="fas fa-edit"></i>&nbsp;Updated At:&nbsp;</strong> {{ $user->updated_at }}</p>
-              </div>
-        </div>
-        </article>
-      </div>
-    </div>
-</div>
       @endforeach
     </div>
 
